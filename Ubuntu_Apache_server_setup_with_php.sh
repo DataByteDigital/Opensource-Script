c073efa3fc2e455feba9ec057924b29c306755dd
Madhurya Dutta <iam.madhurya@outlook.com>
2023-09-02 17:30:52 +0530

    minor update in the apache script

--- Ubuntu_Apache_server_setup_with_php.sh
+++ Ubuntu_Apache_server_setup_with_php.sh
@@ -3,8 +3,7 @@ sudo apt install neofetch -y
 sudo apt install apache2 -y
 sudo ufw app list
 sudo ufw allow 'Apache'
-sudo ufw status
-sudo systemctl status apache2
+
 sudo systemctl restart apache2
 sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
 sudo netfilter-persistent save
@@ -12,10 +11,14 @@ sudo apt install php libapache2-mod-php php-mysql
 php -v
 sudo ufw app list
 sudo ufw allow 'Apache'
-sudo ufw status
 sudo systemctl restart apache2
-hostname -I
-curl -4 icanhazip.com
 sudo apt autoremove
 sudo apt update && sudo apt upgrade -y 
 
+
+
+hostname -I
+curl -4 icanhazip.com
+sudo ufw status
+sudo ufw app list
+sudo systemctl status apache2
