7a250be9c52bdeb4775b0d777fc2d3075fadac7b
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2023-10-28 02:28:55 +0530

    Create simple_set_up.sh

---
+++ simple_set_up.sh
@@ -0,0 +1,10 @@
+sudo apt update && sudo apt upgrade -y 
+sudo apt install neofetch -y
+sudo ufw app list
+yes | sudo ufw enable
+sudo apt update && sudo apt upgrade -y 
+yes | sudo apt autoremove
+sudo ufw status
+sudo ufw app list
+hostname -I
+curl -4 icanhazip.com
