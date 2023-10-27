da649200c7cbc8e3aee4a91b9cf501b2f66f781b
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2023-10-28 02:25:02 +0530

    Update Ubuntu_Apache_server_setup_with_php.sh

--- Ubuntu_Apache_server_setup_with_php.sh
+++ Ubuntu_Apache_server_setup_with_php.sh
@@ -7,7 +7,7 @@ sudo ufw allow 'Apache'
 sudo systemctl restart apache2
 sudo iptables -I INPUT 6 -m state --state NEW -p tcp --dport 80 -j ACCEPT
 sudo netfilter-persistent save
-sudo apt install php libapache2-mod-php php-mysql
+yes | sudo apt install php libapache2-mod-php php-mysql
 php -v
 sudo ufw app list
 sudo ufw allow 'Apache'
@@ -19,6 +19,7 @@ sudo apt update && sudo apt upgrade -y
 
 hostname -I
 curl -4 icanhazip.com
+yes | sudo ufw enable
 sudo ufw status
 sudo ufw app list
 sudo systemctl status apache2
